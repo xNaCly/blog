c7906d304a065509ad201664766f0b64a5f6afa3
xnacly <47723417+xNaCly@users.noreply.github.com>
2025-06-02 18:50:52 +0200

    meta: flake init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "xnacly - blog";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.hugo
+      ];
+    };
+  };
+}
